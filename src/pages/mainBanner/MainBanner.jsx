0209c4d3cbc2d038936d0534b892efa59a9dc879
Olaf Archer <olaf@olaf.com>
2023-01-10 19:58:57 +0200

    main

--- src/pages/mainBanner/MainBanner.jsx
+++ src/pages/mainBanner/MainBanner.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import img1 from "../../assets/mainBanner2.png";
 import img2 from "../../assets/mainBannerLogo.png";
+import {
+  AiOutlineYoutube,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 
 import "./MainBanner.scss";
 
@@ -13,19 +20,33 @@ const MainBanner = () => {
             <img src={img2} alt="#!" />
           </div>
           <div className="main-title">
-            <span>b</span>
-            <span>o</span>
-            <span>o</span>
-            <span>k</span>
-            <span>s</span>
-            <span>t</span>
-            <span>o</span>
-            <span>r</span>
-            <span>e</span>
+            <span className="green">Bo</span>
+            <span className="brown">ok</span>
+            <br />
+            <span className="blue">S</span>
+            <span className="brown">t</span>
+            <span className="green">or</span>
+            <span className="blue">e</span>
           </div>
-          <div className="main-subtitle">blablabla</div>
+
+          <div className="main-subtitle">find new and used books</div>
           <div className="main-text">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+            sit amet consectetur adipisicing
+          </div>
+          <div className="main-social">
+            <Link className="bottom-social-item">
+              <AiOutlineYoutube />
+            </Link>
+            <Link className="bottom-social-item">
+              <AiOutlineFacebook />
+            </Link>
+            <Link className="bottom-social-item">
+              <AiOutlineTwitter />
+            </Link>
+            <Link className="bottom-social-item">
+              <AiOutlineInstagram />
+            </Link>
           </div>
         </div>
         <div className="main-img">
