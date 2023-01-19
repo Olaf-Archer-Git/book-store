135d43c7af205f96a85969e771d53eab6ce4b0d9
olto <olto@gmail.com>
2023-01-19 09:03:26 +0200

    cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Meta from "../../components/meta/Meta";
 import BreadCrumbs from "../../components/breadcrumbs/BreadCrumbs";
+import img1 from "../../assets/book2.png";
+import { TfiTrash } from "react-icons/tfi";
 
 import "./Cart.scss";
 
@@ -12,12 +14,50 @@ const Cart = () => {
       <section className="cart">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-md-12">
+            <div className="col-md-6">
               <div className="cart-container">
-                <div className="cart-product">product</div>
-                <div className="cart-price">price</div>
-                <div className="cart-quantity">quantity</div>
-                <div className="cart-total">total</div>
+                <h4>Product</h4>
+                <div className="cart-box">
+                  <div className="cart-img">
+                    <img src={img1} alt="#!" />
+                  </div>
+                  <div className="cart-content">
+                    <p>Name Of The Book</p>
+                    <p>Name Of The Author</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2">
+              <div className="cart-container">
+                <h4>Price</h4>
+                <div className="cart-box">
+                  <div className="cart-content">$50</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2">
+              <div className="cart-container">
+                <h4>Quantity</h4>
+                <div className="cart-box">
+                  <input
+                    type="number"
+                    className="block-input"
+                    min={1}
+                    max={10}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2">
+              <div className="cart-container">
+                <h4>Total</h4>
+                <div className="cart-box">
+                  <div className="cart-content">
+                    <TfiTrash />
+                    <span>$100</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
