5ddc1d9060fce5c223979e95ab3c79873d4c2b3b
Olaf Archer <olaf@olaf.com>
2023-01-21 12:51:14 +0200

    refactors

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -14,50 +14,45 @@ const ProductCard = ({ sortLayout }) => {
   };
 
   return (
-    <>
-      <div
-        className={`${
-          location.pathname === "/shop" ? `${sortLayout}` : "col-lg-4"
-        }`}
-      >
-        <Link to="/shop/:id">
-          <div className="cards-container">
-            <div className="cards-wrapper">
-              <div className="cards-img">
-                <img className="image" src={img1} alt="#!" />
-              </div>
-              <div title="add to favorite" className="cards-like">
-                <BsSuitHeart />
-              </div>
-            </div>
-            <div className="cards-box">
-              <div className="title">the jungle book</div>
-              <div className="subtitle">rudyard kipling</div>
-              <div
-                className={`cards-description ${
-                  sortLayout === "size-12" ? "d-block" : "d-none"
-                }`}
-              >
-                the book about adventures boy who was named tarzan and another
-                animals
-              </div>
-              <div className="cards-rating">
-                <ReactStars
-                  count={5}
-                  onChange={ratingChanged}
-                  size={21}
-                  activeColor="#ffd700"
-                />
-              </div>
-
-              <div className="btn-main">
-                <Link>add to cart</Link>
-              </div>
-            </div>
+    <div
+      className={`${
+        location.pathname === "/shop" ? `${sortLayout}` : "col-lg-4"
+      }`}
+    >
+      <div className="cards-container">
+        <div className="cards-wrapper">
+          <div className="cards-img">
+            <img className="image" src={img1} alt="#!" />
           </div>
-        </Link>
+          <div title="add to favorite" className="cards-like">
+            <BsSuitHeart />
+          </div>
+        </div>
+        <div className="cards-box">
+          <div className="title">the jungle book</div>
+          <div className="subtitle">rudyard kipling</div>
+          <div
+            className={`cards-description ${
+              sortLayout === "size-12" ? "d-block" : "d-none"
+            }`}
+          >
+            the book about adventures boy who was named tarzan and another
+            animals
+          </div>
+          <div className="cards-rating">
+            <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={21}
+              activeColor="#ffd700"
+            />
+          </div>
+          <div className="btn-main">
+            <Link to="/shop/:id">Details</Link>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
