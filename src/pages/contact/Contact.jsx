0d51f4fde673c307201bbcd575d21127517326f7
olto <olto@gmail.com>
2023-01-22 09:25:11 +0200

    contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
           <div className="col-lg-12">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d1536.2743709749714!2d-97.06636553621361!3d36.74282797890616!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b0f5349a3fa457%3A0xcef42936b02358ae!2sShirlee%20Ave%2C%20Ponca%20City%2C%20OK%2C%20USA!5e1!3m2!1sen!2sua!4v1673547461256!5m2!1sen!2sua"
-              width="750"
+              width="100%"
               height="500"
               style={{ border: "0" }}
               allowFullScreen=""
