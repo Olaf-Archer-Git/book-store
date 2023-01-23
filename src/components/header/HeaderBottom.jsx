df6d196ef3f46d35518926f8faeb7f37c7c8b52b
olto <olto@gmail.com>
2023-01-24 00:05:59 +0200

    ref

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Container from "../container/Container";
 import { CgMenuGridO } from "react-icons/cg";
 import "./Header.scss";
 
 const HeaderBottom = () => {
-  const [open, setOpen] = useState(false);
+  const ref = useRef();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+  useEffect(() => {
+    const getClickOutside = (e) => {
+      if (isOpen && ref.current && !ref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", getClickOutside);
+
+    return () => {
+      document.removeEventListener("click", getClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <div className="header-bottom">
-      <Container className="container-xxl">
+      <Container className={"container-xxl"}>
         <div className="col-md-3 bottom-menu">
           <CgMenuGridO />
-          <div className="dropdown">
-            <button onClick={handleOpen}>Categories</button>
-            {open ? (
+          <div className="dropdown" ref={ref}>
+            <button
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Categories
+            </button>
+            {isOpen && (
               <ul className="menu">
                 <li className="menu-item">
                   <Link to="/shop">Categories 1</Link>
@@ -29,7 +46,7 @@ const HeaderBottom = () => {
                   <Link to="/contact">Categories 3</Link>
                 </li>
               </ul>
-            ) : null}
+            )}
           </div>
         </div>
         <div className="col-md-9">
