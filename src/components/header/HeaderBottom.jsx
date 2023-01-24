baf549c910be144d9e9ce97837b63a38ca38729a
olto <olto@gmail.com>
2023-01-24 11:02:23 +0200

    header

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -24,46 +24,38 @@ const HeaderBottom = () => {
   return (
     <div className="header-bottom">
       <Container className={"container-xxl"}>
-        <div className="col-md-3 bottom-menu">
-          <CgMenuGridO />
-          <div className="dropdown" ref={ref}>
-            <button
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Categories
-            </button>
-            {isOpen && (
-              <ul className="menu">
-                <li className="menu-item">
-                  <Link to="/shop">Categories 1</Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/blog">Categories 2</Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/contact">Categories 3</Link>
-                </li>
-              </ul>
-            )}
+        <div className="col-lg-3">
+          <div className="bottom-container">
+            <div className="dropdown" ref={ref}>
+              <CgMenuGridO />
+              <button
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Categories
+              </button>
+              {isOpen && (
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item">
+                    <Link to="/shop">Categories 1</Link>
+                  </li>
+                  <li className="dropdown-item">
+                    <Link to="/blog">Categories 2</Link>
+                  </li>
+                  <li className="dropdown-item">
+                    <Link to="/contact">Categories 3</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
           </div>
         </div>
-        <div className="col-md-9">
-          <ul className="bottom-list">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop</Link>
-            </li>
-          </ul>
+        <div className="col-lg-9">
+          <div className="bottom-container">
+            <div>2</div>
+            <div>2</div>
+          </div>
         </div>
       </Container>
     </div>
@@ -71,3 +63,49 @@ const HeaderBottom = () => {
 };
 
 export default HeaderBottom;
+
+// <div className="header-bottom">
+//     <Container className={"container-xxl"}>
+//       <div className="col-md-3 bottom-menu">
+//         <div className="dropdown" ref={ref}>
+//           <CgMenuGridO />
+//           <button
+//             onClick={() => {
+//               setIsOpen(!isOpen);
+//             }}
+//           >
+//             Categories
+//           </button>
+//           {isOpen && (
+//             <ul className="menu">
+//               <li className="menu-item">
+//                 <Link to="/shop">Categories 1</Link>
+//               </li>
+//               <li className="menu-item">
+//                 <Link to="/blog">Categories 2</Link>
+//               </li>
+//               <li className="menu-item">
+//                 <Link to="/contact">Categories 3</Link>
+//               </li>
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//       <div className="col-md-9">
+//         <ul className="bottom-list">
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//           <li>
+//             <Link to="/blog">Blog</Link>
+//           </li>
+//           <li>
+//             <Link to="/shop">Shop</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </Container>
+//   </div>
